a6be3409cb5e3c768849ae809e4b8c1b2790e3b5
RolandSall <roland.salloum00@outlook.com>
2023-05-07 13:23:09 +0300

    feat: setup postgres container for command and query services

---
+++ inventory-service/inventory-query/src/main/java/com/rolandsall/inventory/query/InventoryQueryServiceLauncher.java
@@ -0,0 +1,11 @@
+package com.rolandsall.inventory.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryQueryServiceLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryQueryServiceLauncher.class, args);
+    }
+}
